c569d1c5c5d2cb7a47a9a939bea0ba57e14144d4
Kyrylo Kirpenko <k.v.kirpenko@gmail.com>
2021-02-11 21:37:05 +0200

    logOut

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -6,8 +6,23 @@ import SearchIcon from '@material-ui/icons/Search';
 import ArrowDropDownIcon from '@material-ui/icons/ArrowDropDown';
 import AppsIcon from '@material-ui/icons/Apps';
 import NotificationsIcon from '@material-ui/icons/Notifications';
+import {useDispatch, useSelector} from "react-redux";
+import {logout, selectUser} from "../../features/userSlice";
+import {auth} from "../../firebase";
 
 function Header() {
+
+    const user = useSelector(selectUser)
+
+    const dispatch = useDispatch()
+
+    const signOut = () => {
+        auth.signOut().then( () => {
+            dispatch(logout())
+        })
+    }
+
+
     return (
         <div className='header'>
             <div className="header__left">
@@ -30,7 +45,7 @@ function Header() {
                 <IconButton>
                     <NotificationsIcon/>
                 </IconButton>
-                <Avatar/>
+                <Avatar src={user?.photoURL} onClick={signOut} />
             </div>
         </div>
     )
